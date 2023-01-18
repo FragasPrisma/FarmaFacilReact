e9343d8ce01be4f25f2bc757396fbc46cb8dc071
bruno <bruno@prismafive.com.br>
2023-01-18 15:05:50 -0300

    request api melhorada

--- FarmaFacilReact/src/Services/Api.tsx
+++ FarmaFacilReact/src/Services/Api.tsx
@@ -7,6 +7,20 @@ export const createSession = async (email: string, password: string) => {
   return api.post("CreateToken", { email, password });
 };
 
-export const getAll = async ( url: string): Promise<any> => {
-  return api.get(url);
+export const getAll = async (url: string): Promise<any> => {
+  try{
+    const response = await api.get(url);
+    return response
+  }catch(error){
+    throw new Error("Erro ao buscar dados");
+  }
+};
+
+export const postFormAll = async (url: string, payload: any): Promise<JSON> => {
+  try {
+    const response = await api.post(url, payload);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Erro ao enviar dados para ${url}. Erro: ${error}`);
+  }
 };
