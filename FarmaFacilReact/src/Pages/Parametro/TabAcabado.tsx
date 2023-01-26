2a3c39cbd9d66fc573485843a1105787c487e24d
Bruno <brunobento@prismafive.com.br>
2023-01-26 10:08:46 -0300

    Adicionado PagTab da tela de parametro

---
+++ FarmaFacilReact/src/Pages/Parametro/TabAcabado.tsx
@@ -0,0 +1,17 @@
+import { CustomInput } from "../../Components/Inputs/CustomInput";
+
+export const TabAcabado = () => {
+    return (
+        <>
+            <div className="row">
+                <div className="col-2">
+                    <CustomInput
+                        label="Caminho salvar NFe"
+                        required={false}
+                        type="string"
+                    />
+                </div>
+            </div>
+        </>
+    );
+};
