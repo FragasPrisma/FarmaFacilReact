69004235cfd0c5d590936ad77a52d25c1d3b012f
FragasPrisma <andre.fragas@prismafive.com.br>
2023-03-06 16:31:34 -0300

    Index Compras

---
+++ FarmaFacilReact/src/Enum/StatusCompra.ts
@@ -0,0 +1,7 @@
+export enum StatusCompra {
+    TODOS = 0,
+    EMABERTO = 1,
+    PARCIAL = 2,
+    COMPLETO = 3,
+    CANCELADO = 4
+}
---
+++ FarmaFacilReact/src/Enum/TipoCompra.ts
@@ -0,0 +1,8 @@
+export enum TipoCompra {
+    VENDA = 1,
+    DEMANDA = 2,
+    ESTOQUE_MINIMO = 3,
+    ESTOQUE_MAXIMO = 4,
+    CONSUMO = 5,
+    ENCOMENDA_FALTAS = 6,
+}
