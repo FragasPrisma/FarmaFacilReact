77345ffa78ac9ae6789a5231ca22695a6dabf6ff
FragasPrisma <andre.fragas@prismafive.com.br>
2023-03-08 17:27:04 -0300

    Compras visualization

--- FarmaFacilReact/src/Pages/Compras/ComprasDetails.tsx
+++ FarmaFacilReact/src/Pages/Compras/ComprasDetails.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, ThemeProvider } from "@mui/material";
 import { DataGrid, GridToolbarContainer, GridToolbarDensitySelector, GridToolbarFilterButton } from '@mui/x-data-grid'
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -7,35 +7,26 @@ import { CheckboxCustom } from "../../Components/Inputs/CheckboxCustom";
 import { CustomInput } from "../../Components/Inputs/CustomInput";
 import { RadioCustom } from "../../Components/Inputs/RadioCustom";
 import { FieldsetCustom } from "../../Components/Others/FieldsetCustom";
-import { SetDataMultiSelect } from "../../helper/GerarDataMultiSelect";
 import { IItemsCompra } from "../../Interfaces/Compras/IItemsCompra";
 import { IFiltroCompras } from "../../Interfaces/Compras/IFiltroCompras";
-import { IFornecedor } from "../../Interfaces/Fornecedor/IFornecedor";
-import { IGrupo } from "../../Interfaces/Grupo/IGrupo";
 import { GetId } from "../../Services/Api";
-import { Container } from "./styles";
+import { Container, FooterGridTotal } from "./styles";
+import { setTheme, setTranslate } from "../../helper/GridsTranslate/TranslateFunctions";
+import { getCurvaAbcString, getTipoCompraString } from "../../helper/TransformaEnum";
 
 export function ManutencaoComprasDetails() {
     const [filtro, setFiltro] = useState({} as IFiltroCompras)
 
-    const [fornecedores, setFornecedores] = useState([] as IFornecedor[]);
-    const [grupos, setGrupos] = useState([] as IGrupo[]);
-    //const [produtos, setProdutos] = useState([] as IProduto[]);
-
-    const [descricaoLaboratorio, setDescricaoLaboratorio] = useState(0);
+    const [descricaoLaboratorio, setDescricaoLaboratorio] = useState("");
 
     const [itemsCompras, setItemsCompras] = useState([] as IItemsCompra[]);
 
-    const columns = [
-        { field: "id", headerName: "Id", width: 60 },
-        { field: "codigogrupo", headerName: "Código Grupo", width: 150 },
-        { field: "estoque", headerName: "Estoque", width: 200 },
-        { field: "descricaoproduto", headerName: "Descrição Produto", width: 200 },
-        { field: "nomelaboratorio", headerName: "Nome Laboratório", width: 200 },
-        { field: "curvaabcproduto", headerName: "Curva Abc Produto", width: 200 },
-        { field: "estoqueminimoproduto", headerName: "Estoque Minimo Produto", width: 200 },
-        { field: "estoquemaximoproduto", headerName: "Estoque Máximo Produto", width: 200 },
-    ];
+    const [hideUnidade, setHideUnidade] = useState(false);
+    const [hideQuantidadeTotal, setHideQuantidadeTotal] = useState(false);
+    const [hideConsumoDiario, setHideConsumoDiario] = useState(false);
+    const [hideLaboratorio, setHideLaboratorio] = useState(true);
+
+    const [valorTotal, setValorTotal] = useState(0);
 
     function CustomToolbar() {
         return (
@@ -46,6 +37,116 @@ export function ManutencaoComprasDetails() {
         );
     }
 
+    const columns = [
+        {
+            field: "id",
+            headerName: "Id",
+            width: 60,
+        },
+        {
+            field: "grupoId",
+            headerName: "Grupo",
+            width: 150
+        },
+        {
+            field: "produtoId",
+            headerName: "Produto",
+            width: 200
+        },
+        {
+            field: "laboratorioId",
+            headerName: "Laboratorio",
+            width: 200
+        },
+        {
+            field: "siglaUnidade",
+            headerName: "Unidade",
+            width: 200
+        },
+        {
+            field: "curva",
+            headerName: "Curva",
+            width: 200
+        },
+        {
+            field: "estoqueMinimo",
+            headerName: "Estoque Minimo",
+            width: 200
+        },
+        {
+            field: "estoqueMaximo",
+            headerName: "Estoque Máximo",
+            width: 200
+        },
+        {
+            field: "quantidadeVendida",
+            headerName: "Quantidade Vendida",
+            width: 200
+        },
+        {
+            field: "valorVendido",
+            headerName: "Valor Vendido",
+            width: 200
+        },
+        {
+            field: "estoque",
+            headerName: "Estoque",
+            width: 200
+        },
+        {
+            field: "quantidadeCompra",
+            headerName: "Quantidade Compra",
+            width: 200,
+            type: "number",
+        },
+        {
+            field: "quantidadeTotal",
+            headerName: "Quantidade Total",
+            width: 200
+        },
+        {
+            field: "consumoDiario",
+            headerName: "Consumo Diario",
+            width: 200
+        },
+        {
+            field: "valorUnitario",
+            headerName: "Valor Unitario",
+            width: 200
+        },
+        {
+            field: "valorTotal",
+            headerName: "Valor Total",
+            width: 200
+        },
+        {
+            field: "comprar",
+            headerName: "Comprar",
+            width: 200,
+            type: 'boolean',
+        },
+        {
+            field: "nomeFornecedor",
+            headerName: "Fornecedor Ultima Compra",
+            width: 200,
+        },
+        {
+            field: "codigoCas",
+            headerName: "Código CAS",
+            width: 200,
+        },
+        {
+            field: "codigoDcb",
+            headerName: "Código DCB",
+            width: 200,
+        },
+        {
+            field: "codigoBarra",
+            headerName: "Código Barra",
+            width: 200,
+        }
+    ];
+
     const { id } = useParams();
     let idParams = !id ? "0" : id.toString();
 
@@ -55,23 +156,52 @@ export function ManutencaoComprasDetails() {
             const response = await GetId("RetornaCompraPorId", idParams);
 
             if (response.status === 200) {
-                if (response.data.laboratorio) {
+                if (response.data.laboratorioId > 0) {
                     setDescricaoLaboratorio(response.data.laboratorio.descricao)
                 }
 
-                // if (response.data.fornecedores.length > 0) {
-                //     setFornecedores(SetDataMultiSelect(response.data.fornecedores, "nomeFornecedor"))
-                // }
-
-                // if (response.data.grupos.length > 0) {
-                //     setFornecedores(SetDataMultiSelect(response.data.grupos, "descricao"))
-                // }
-
-                // if (response.data.produtos.length > 0) {
-                //     setProdutos(SetDataMultiSelect(response.data.produtos, "nome"))
-                // }
-
                 setFiltro(response.data);
+                setItemsCompras(response.data.itensCompras);
+                setValorTotal(response.data.totalCompra);
+
+                switch (response.data.tipoCompra) {
+                    case (response.data.tipoCompra == 1) :
+                        setHideLaboratorio(true);
+                        setHideUnidade(false);
+                        setHideQuantidadeTotal(false);
+                        setHideConsumoDiario(false);
+                        break;
+                    case (response.data.tipoCompra == 2) :
+                        setHideLaboratorio(true);
+                        setHideUnidade(false);
+                        setHideQuantidadeTotal(false);
+                        setHideConsumoDiario(false);
+                        break;
+                    case (response.data.tipoCompra == 3) :
+                        setHideLaboratorio(true);
+                        setHideUnidade(false);
+                        setHideQuantidadeTotal(false);
+                        setHideConsumoDiario(false);
+                        break;
+                    case (response.data.tipoCompra == 4) :
+                        setHideLaboratorio(true);
+                        setHideUnidade(false);
+                        setHideQuantidadeTotal(false);
+                        setHideConsumoDiario(false);
+                        break; 
+                    case (response.data.tipoCompra == 5) :
+                        setHideLaboratorio(false);
+                        setHideUnidade(true);
+                        setHideQuantidadeTotal(true);
+                        setHideConsumoDiario(true);
+                        break;
+                    case (response.data.tipoCompra == 6) :
+                        setHideLaboratorio(true);
+                        setHideUnidade(false);
+                        setHideQuantidadeTotal(false);
+                        setHideConsumoDiario(false);
+                        break;
+                }
             }
         }
 
@@ -80,14 +210,14 @@ export function ManutencaoComprasDetails() {
 
     return (
         <>
-            <HeaderMainContent title="Visualizar Manutenção de Compra" IncludeButton={false} ReturnButton={true} to="compras" />
+            <HeaderMainContent title="Visualizar Compra" IncludeButton={false} ReturnButton={true} to="compras"/>
             <Container>
                 <div className="row">
                     <div className="col-2 mt-4">
                         <CustomInput
                             label="Tipo"
                             type="text"
-                            value={filtro.tipoCompra}
+                            value={getTipoCompraString(filtro.tipoCompra)}
                             readonly={true}
                         />
                     </div>
@@ -97,52 +227,36 @@ export function ManutencaoComprasDetails() {
                                 name="Tipo Demanda"
                                 options={["Estoque Mínimo", "Estoque Máximo"]}
                                 titleComponet="Tipo Demanda"
-                                value={filtro.tipoDemanda ? filtro.tipoDemanda : null}
                                 readonly={true}
+                                value={filtro.tipoDemanda}
                             />
                         }
                     </div>
                     <div className="col-2 mt-4">
                         <CustomInput
                             label="Venda de"
-                            type="date"
+                            type="datetime-local"
                             value={filtro.vendaDe}
-                            readonly={true}
-                        />
-                    </div>
-                    <div className="col-1 mt-4">
-                        <CustomInput
-                            label="Hora"
-                            type="time"
-                            value={filtro.vendaDeHora}
                             readonly={true}
                         />
                     </div>
                     <div className="col-2 mt-4">
                         <CustomInput
                             label="Venda até"
-                            type="date"
+                            type="datetime-local"
                             value={filtro.vendaAte}
                             readonly={true}
                         />
                     </div>
-                    <div className="col-1 mt-4">
-                        <CustomInput
-                            label="Hora"
-                            type="time"
-                            value={filtro.vendaAteHora}
-                            readonly={true}
-                        />
-                    </div>
-                    <div className="col-1 mt-4">
+                    <div className="col-2 mt-4">
                         <CustomInput
                             label="Tempo de Rep"
                             type="number"
-                            value={filtro.tempoDeRep}
+                            value={filtro.tempoDeReposicao}
                             readonly={true}
                         />
                     </div>
-                    <div className="col-1 mt-4">
+                    <div className="col-2 mt-4">
                         <CustomInput
                             label="Quantidade de dias"
                             type="number"
@@ -156,14 +270,14 @@ export function ManutencaoComprasDetails() {
                         <CustomInput
                             label="Curva Abc"
                             type="text"
-                            value={filtro.curvaAbc}
+                            value={getCurvaAbcString(filtro.curvaAbc)}
                             readonly={true}
                         />
                     </div>
                     <div className="col-2">
 
                     </div>
-                    <div className="col-2">
+                    <div className="col-4">
                         <CustomInput
                             label="Laboratório"
                             type="text"
@@ -171,19 +285,14 @@ export function ManutencaoComprasDetails() {
                             readonly={true}
                         />
                     </div>
-                    <div className="col-2 mt-3">
-                        <CheckboxCustom
-                            options={["Considerar Encomenda/Faltas"]}
-                            check={filtro.consideraEncomendaFaltas}
-                            readOnly={true}
-                        />
-                    </div>
-                    <div className="col-3 mt-3">
-                        <CheckboxCustom
-                            options={["Saldo com Quantidade Comprometida"]}
-                            check={filtro.saldoQuantidadeComprometida}
-                            readOnly={true}
-                        />
+                    <div className="col-4">
+                        {/* { <CustomDropDown
+                            data={empresas}
+                            title="Selecione a Empresa"
+                            filter="nome"
+                            label="Empresa"
+                            Select={(empresaId) => setEmpresaId(empresaId)}
+                        /> } */}
                     </div>
                 </div>
                 <div className="row">
@@ -192,8 +301,8 @@ export function ManutencaoComprasDetails() {
                             name="Tipo Valor"
                             options={["Custo Referência", "Última Compra", "Melhor Compra"]}
                             titleComponet="Tipo Valor"
-                            value={filtro.tipoValor}
                             readonly={true}
+                            value={filtro.tipoValor}
                         />
                     </div>
                     <div className="col-2 mt-4">
@@ -204,52 +313,53 @@ export function ManutencaoComprasDetails() {
                             readonly={true}
                         />
                     </div>
-                    <div className="col-4 mt-4">
-                        {/* <MultiSelect
-                            label="Grupos"
-                            title="Grupos" 
-                            data={grupos}
-                            isMultiple={true}
-                            Select={(gruposIds) => setGruposIds(gruposIds)}
-                            placeholder="Selecione o(s) grupo(s)"
-                        /> */}
-                    </div>
-                    <div className="col-4 mt-4">
-                        {/* <MultiSelect 
-                            label="Fornecedores"
-                            title="Fornecedores" 
-                            data={fornecedores}
-                            isMultiple={true}
-                            Select={(fornecedoresIds) => setFornecedoresIds(fornecedoresIds)}
-                            placeholder="Selecione o(s) fornecedor(es)"
-                        /> */}
-                    </div>
-                </div>
-                <div className="row">
-
-                    <div className="col-4">
-                        {/* <MultiSelect 
-                            label="Produtos"
-                            title="Produtos"
-                            data={produtos}
-                            isMultiple={true}
-                            Select={(produtosIds) => SetProdutosIds)}
-                            placeholder="Selecione o(s) produto(s)"
-                        /> */}
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-12 mt-4">
-                        <FieldsetCustom legend="Itens Compra">
-                            <section>
-                                <Box sx={{ height: 400, mt: 1 }}>
-                                    <DataGrid rows={itemsCompras} columns={columns} editMode="row" components={{ Toolbar: CustomToolbar }} />
-                                </Box>
-                            </section>
-                        </FieldsetCustom>
+                    <div className="col-4 mt-3">
+                        <CheckboxCustom
+                            options={["Considerar apenas empresa selecionada"]}
+                            check={filtro.considerarApenasEmpresaSelecionada}
+                            readOnly={true}
+                        />
+                        <CheckboxCustom
+                            options={["Considerar Encomenda/Faltas"]}
+                            check={filtro.consideraEncomendaFaltas}
+                            readOnly={true}
+                        />
+                        <CheckboxCustom
+                            options={["Saldo com Quantidade Comprometida"]}
+                            check={filtro.saldoQuantidadeComprometida}
+                            readOnly={true}
+                        />
                     </div>
                 </div>
             </Container>
+            <div className="row">
+                <div className="col-12 mt-4">
+                    <FieldsetCustom legend="Itens Compra">
+                        <section>
+                            <ThemeProvider theme={setTheme()}>
+                                <Box sx={{ height: 400, mt: 1 }}>
+                                    <DataGrid
+                                        rows={itemsCompras}
+                                        columns={columns}
+                                        components={{
+                                            Toolbar: CustomToolbar,
+                                        }}
+                                        localeText={setTranslate()}
+                                        columnVisibilityModel={{
+                                            id: false,
+                                            laboratorioId: hideLaboratorio,
+                                            siglaUnidade: hideUnidade,
+                                            quantidadeTotal: hideQuantidadeTotal,
+                                            consumoDiario: hideConsumoDiario,
+                                        }} 
+                                    />
+                                    <FooterGridTotal>Valor total: {valorTotal}</FooterGridTotal>
+                                </Box>
+                            </ThemeProvider>
+                        </section>
+                    </FieldsetCustom>
+                </div>
+            </div>
         </>
     )
 }
